18bfc03bda27134b04c0fee22db7f19a349c7cdf
Dmitriy_Demin <d.demin@inbox.ru>
2024-05-14 19:37:36 +0400

    sort_merge

---
+++ task_Merge_sort.java
@@ -0,0 +1,40 @@
+import java.sql.SQLOutput;
+
+public class task_Merge_sort  {
+    public static void main(String[] args) {
+        int [] ArrNum = new int[]{3,1,7,5};
+        splitArr (ArrNum);
+
+    }
+    public int[] splitArr (int [] Arr){
+        if (Arr == null)
+            return null;
+        if (Arr.length < 2)
+            return Arr;
+        int [] ArrA = new int [Arr.length/2];
+        int [] ArrB = new int [Arr.length - Arr.length/2];
+        int j, k = 0;
+        for (int i = 0; i < Arr.length; i++){
+            if (i < Arr.length/2){
+                ArrA[j] = Arr[i];
+                j++;
+            } else {
+                ArrB[k] = Arr[i];
+                k++;
+            }
+        }
+        ArrA = splitArr(ArrA);
+        ArrB = splitArr(ArrB);
+
+        return mergeArr(ArrA, ArrB);
+
+    }
+    public int [] mergeArr (int [] Arr1, int [] Arr2){
+        int [] Arr3 = new int [Arr1.length + Arr2.length];
+        return Arr3;
+
+
+
+    }
+
+}
